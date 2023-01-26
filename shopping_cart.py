e0decdb72f38eefa468f4f90e9b9b5de307e7005
Hasmik Melkonyan <82414047+hasmelkonyan@users.noreply.github.com>
2023-01-26 12:48:36 +0400

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,20 @@
 from product import Product
+import werehouse
 
 
 class ShoppingCart:
     def __init__(self):
         self.__products = []
 
-    def add_product(self, product):
+    def add_to_shopping_cart(self, product):
         if isinstance(product, Product):
-            self.__products.append(product)
+            self.__products.append(product.get_product_name())
+            werehouse.werehouse_dct[product.get_product_name()] -= 1
 
-    def remove_product(self, product):
+    def remove_from_shopping_cart(self, product):
         if isinstance(product, Product) and product in self.__products:
-            self.__products.remove(product)
+            self.__products.remove(product.get_product_name())
+            werehouse.werehouse_dct[product.get_product_name()] += 1
 
     def total_price(self):
         total = 0
@@ -24,3 +27,4 @@ class ShoppingCart:
         for product in self.__products:
             all.append((product.get_product_name(), product.get_price()))
         return all
+
